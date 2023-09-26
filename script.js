05f62932847e890c789055038747735c0442fb17
ArthurAMata <aamata2017@gmail.com>
2023-09-26 19:15:30 -0300

    JABULANI

--- script.js
+++ script.js
@@ -87,3 +87,27 @@ const loop = setInterval(() => {
         clearInterval(loop);
     }
 }, 10);
+
+
+
+
+
+const loopletter = setInterval(() => {
+    const letterposicao = letter.offsetLeft;
+    if(letterposicao === 0){
+        
+    }
+
+    if (posicaotubo <= 90 && posicaotubo > 0 && marioPosicao < 80) {
+        tubo.style.animation = 'none';
+        tubo.style.left = `${posicaotubo}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosicao}px`;
+
+        mario.src = 'imgs/morte.png';
+        mario.style.width = '150px';
+
+        clearInterval(loop);
+    }zzzzz
+}, 10);
